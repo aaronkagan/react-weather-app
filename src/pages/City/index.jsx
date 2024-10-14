8e28fb3146f81bc59bd2fef90390a559424da9ce
Aaron Kagan <aaronkagan@gmail.com>
2024-10-14 00:39:46 -0400

    removing unused state

--- src/pages/City/index.jsx
+++ src/pages/City/index.jsx
@@ -9,15 +9,12 @@ export default function City({
   },
 }) {
   const [weatherData, setWeatherData] = useState();
-  const [coords, setCoords] = useState();
 
   async function getWeatherData() {
     const { data: coords } = await axios(
       `https://api.openweathermap.org/geo/1.0/direct?q=${cityId}&limit=5&appid=${openWeatherApiKey}`
     );
     const { lat, lon } = coords[0];
-
-    setCoords({ lat, lon });
 
     const { data } = await axios(
       `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${openWeatherApiKey}`
