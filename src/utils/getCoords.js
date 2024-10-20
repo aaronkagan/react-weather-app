4496807dc667c657129ffca12766feab094738d1
Aaron Kagan <aaronkagan@gmail.com>
2024-10-20 10:34:13 -0400

    removing try catch

--- src/utils/getCoords.js
+++ src/utils/getCoords.js
@@ -3,13 +3,9 @@ import axios from 'axios';
 const openWeatherApiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
 
 export default async function getCoords(city) {
-  try {
-    const { data: coords } = await axios(
-      `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${openWeatherApiKey}`
-    );
+  const { data: coords } = await axios(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${openWeatherApiKey}`
+  );
 
-    return coords[0];
-  } catch (error) {
-    console.log(error);
-  }
+  return coords[0];
 }
